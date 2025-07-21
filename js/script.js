23a8d57c6a9d3f293b44491189523585622397ef
Liliia Galkina <vvlili@yandex.ru>
2025-07-21 16:16:25 +0300

    адаптив

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+'use strict'
+
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+
+burger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+   burger.classList.toggle("menu__burger_active");
+	menu.classList.toggle("menu__list_active");
+}
+
+
+function mobileMenuItem(){
+    menu.classList.remove("menu__list_active")
+    burger.classList.remove("menu__burger_active")
+}
+
+const menuItems = document.querySelectorAll(".menu__item");
+
+for(let item of menuItems) {
+    item.addEventListener("click", mobileMenuItem);
+}
+
