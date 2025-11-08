245bf908ee7912f5524bc715dd2d4b8f56fb80b0
Liliia Galkina <vvlili@yandex.ru>
2025-11-08 13:57:34 +0300

    исправление стилей

--- js/script.js
+++ js/script.js
@@ -1,26 +1,23 @@
-
-'use strict'
+"use strict";
 
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
 
-
-burger.addEventListener('click', mobileMenu);
+burger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-   burger.classList.toggle("menu__burger_active");
-	menu.classList.toggle("menu__list_active");
+  burger.classList.toggle("menu__burger_active");
+  menu.classList.toggle("menu__list_active");
 }
 
-
-function mobileMenuItem(){
-    menu.classList.remove("menu__list_active")
-    burger.classList.remove("menu__burger_active")
+function mobileMenuItem() {
+  menu.classList.remove("menu__list_active");
+  burger.classList.remove("menu__burger_active");
 }
 
 const menuItems = document.querySelectorAll(".menu__item");
 
-for(let item of menuItems) {
-    item.addEventListener("click", mobileMenuItem);
+for (let item of menuItems) {
+  item.addEventListener("click", mobileMenuItem);
 }
 
